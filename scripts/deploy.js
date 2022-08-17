c4dcd190702481ba6dd5ade1e18f83d31b828858
952355188@qq.com <952355188@qq.com>
2022-08-17 22:26:13 +0200

    update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,55 +5,74 @@ async function main() {
 
   // This is just a convenience check
   if (network.name === "hardhat") {
-   console.warn(
-     "You are trying to deploy a contract to the Hardhat Network, which" +
-       "gets automatically created and destroyed every time. Use the Hardhat" +
-       " option '--network localhost'"
-   );
-   }
- // ethers is available in the global scope
- const [deployer] = await ethers.getSigners();
- console.log(
-   "Deploying the contracts with the account:",
-   await deployer.getAddress()
- );
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+      "gets automatically created and destroyed every time. Use the Hardhat" +
+      " option '--network localhost'"
+    );
+  }
+  // ethers is available in the global scope
+  const [deployer] = await ethers.getSigners();
+  console.log(
+    "Deploying the contracts with the account:",
+    await deployer.getAddress()
+  );
 
- const MyFT = await ethers.getContractFactory("ACoin")
- 
- // Start deployment, returning a promise that resolves to a contract object
- const myFT = await MyFT.deploy()
- await myFT.deployed()
- console.log("Contract deployed to address:", myFT.address)
+  const MyFT = await ethers.getContractFactory("ACoin")
 
- saveFrontendFiles(myFT);
+  // Start deployment, returning a promise that resolves to a contract object
+  const myFT = await MyFT.deploy()
+  await myFT.deployed()
+  console.log("Contract deployed to address:", myFT.address)
+
+  saveFrontendFiles(myFT);
 
 }
 
 function saveFrontendFiles(token) {
- const fs = require("fs");
- const contractsDir = path.join(__dirname, "..", "/contracts");
+  const fs = require("fs");
+  const contractsDir = path.join(__dirname, "..", "/contracts/contract_address");
 
- if (!fs.existsSync(contractsDir)) {
-   fs.mkdirSync(contractsDir);
- }
+  if (!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir);
+  }
 
- fs.writeFileSync(
-   path.join(contractsDir, "contract_address.json"),
-   JSON.stringify({ Token: token.address }, undefined, 2)
- );
+  fs.writeFileSync(
+    path.join(contractsDir, "contract_address.json"),
+    JSON.stringify({ Token: token.address }, undefined, 2)
+  );
 
- const TokenArtifact = artifacts.readArtifactSync("ACoin");
+  const TokenArtifact = artifacts.readArtifactSync("ACoin");
 
- fs.writeFileSync(
-   path.join(contractsDir, "ACoin.json"),
-   JSON.stringify(TokenArtifact, null, 2)
- );
- }
- 
- main()
-   .then(() => process.exit(0))
-   .catch((error) => {
-     console.error(error)
-     process.exit(1)
-   })
- 
+  fs.writeFileSync(
+    path.join(contractsDir, "ACoin.json"),
+    JSON.stringify(TokenArtifact, null, 2)
+  );
+}
+
+main().then(() => process.exit(0)).catch((error) => {
+  console.error(error)
+  process.exit(1)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
