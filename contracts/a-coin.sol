1c52f99511423676167d7227a1f553917c7feb60
Yufei Yuan <yuanyufei1999@gmail.com>
2022-08-18 14:26:18 +0200

    Add refund (to return the extra fund)

--- contracts/a-coin.sol
+++ contracts/a-coin.sol
@@ -23,6 +23,8 @@ contract ACoin is ERC721URIStorage, Ownable {
 
     // The max uint256 value of solidity
     uint256 private constant MAX_INT = 2**256 - 1;
+    uint256 private constant NUM_CCOIN_TO_WAIVE_COMMISSION = 100;
+    uint256 private constant COMMISSION_PERCENTAGE = 1000;
 
     // CCoin instance to access the CCoin contract
     CCoin private cCoin;
@@ -48,57 +50,78 @@ contract ACoin is ERC721URIStorage, Ownable {
     }
 
     // To transfer the token ownership to another address, the owner need to pay for the commission.
-    function transfer(address recipient, uint256 tokenId) public payable {
+    function transfer(address recipient, uint256 tokenId)
+        public
+        payable
+        returns (bool)
+    {
         _requireMinted(tokenId);
         require(
             _msgSender() != ownerOf(tokenId),
             "You cannot transfer your token to yourself"
         );
 
-        uint commission = _prices[tokenId] / uint(1000);
-        console.log("The commission of this transfer is: ", commission);
+        uint commission = _prices[tokenId] / COMMISSION_PERCENTAGE;
 
         require(
             msg.value >= _prices[tokenId] + commission,
-            "You do not have enough ether to pay for the commission"
+            "You do not have enough ether to pay"
         );
+
         _forSale[tokenId] = false;
         safeTransferFrom(_msgSender(), recipient, tokenId);
+
+        // Refund the extra ETH to the buyer
+        uint extraFund = msg.value - _prices[tokenId];
+        bool waiveCommission = cCoin.totalBalance(_msgSender()) >=
+            NUM_CCOIN_TO_WAIVE_COMMISSION;
+        if (waiveCommission) {
+            cCoin.reduceBalance(_msgSender(), NUM_CCOIN_TO_WAIVE_COMMISSION);
+        } else {
+            extraFund -= commission;
+        }
+        if (extraFund > 0) {
+            payable(_msgSender()).transfer(extraFund);
+        }
+        return waiveCommission;
     }
 
     function purchase(uint256 tokenId) public payable returns (bool) {
         _requireMinted(tokenId);
         require(_forSale[tokenId], "This token is not for sale");
+        address buyer = _msgSender();
+        require(buyer != ownerOf(tokenId), "You can't purchase your own token");
+
+        uint commission = _prices[tokenId] / COMMISSION_PERCENTAGE;
         require(
-            _msgSender() != ownerOf(tokenId),
-            "You can't purchase your own token"
+            msg.value >= _prices[tokenId] + commission,
+            "You do not have enough ether to pay"
         );
 
-        uint commission = _prices[tokenId] / uint(1000);
-        console.log("The commission of this purchase is: ", commission);
-
-        if (cCoin.totalBalance(msg.sender) < 100) {
-            require(
-                msg.value >= _prices[tokenId] + commission,
-                "You do not have enough ether to pay for the commission"
-            );
-        }
-        require(msg.value >= _prices[tokenId], "You don't have enough money");
-
+        // Get ready to transfer the token
         _forSale[tokenId] = false;
 
-        // Transfer the ETH to the NFT owner
-        if (cCoin.totalBalance(msg.sender) >= 100) {
-            cCoin.reduceBalance(msg.sender, 100);
-        }
+        // Transfer the ETH to the original NFT owner
         payable(ownerOf(tokenId)).transfer(_prices[tokenId]);
 
         // Transfer the NFT ownership to the buyer
         _transfer(ownerOf(tokenId), _msgSender(), tokenId);
-
         // The buyer will get a CCoin as bonus
-        cCoin.mintFT(msg.sender);
-        return true;
+        cCoin.mintFT(buyer);
+
+        // Refund the extra ETH to the buyer
+        uint extraFund = msg.value - _prices[tokenId];
+        bool waiveCommission = cCoin.totalBalance(buyer) >=
+            NUM_CCOIN_TO_WAIVE_COMMISSION;
+        if (waiveCommission) {
+            cCoin.reduceBalance(buyer, NUM_CCOIN_TO_WAIVE_COMMISSION);
+        } else {
+            extraFund -= commission;
+        }
+        if (extraFund > 0) {
+            payable(buyer).transfer(extraFund);
+        }
+        return waiveCommission;
     }
 
     function getContractOwner() public view returns (address) {
@@ -136,9 +159,23 @@ contract ACoin is ERC721URIStorage, Ownable {
         _prices[tokenId] = price * 1e18;
     }
 
+    // Interfaces for the CCoin Contract
+
     // Get the CCoin Contract Address
     function getCCoinContractAddress() public view returns (address) {
         return address(cCoin);
+    }
+
+    function cCoinBalanceOf() public view returns (uint256) {
+        return cCoin.balanceOf(_msgSender());
+    }
+
+    function cCoinTotalBlanceOf() public view returns (uint256) {
+        return cCoin.totalBalance(_msgSender());
+    }
+
+    function cCoinAmountOf() public view returns (uint256) {
+        return cCoin.amountOf(_msgSender());
     }
 
     event Received(address, uint);
