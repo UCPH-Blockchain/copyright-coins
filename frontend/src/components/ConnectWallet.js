92b82151d31b49e57d8fe762ab3f50e1b5a21369
leafff7 <leafff7@163.com>
2022-08-19 21:56:44 +0200

    update page of the connetWallet

--- frontend/src/components/ConnectWallet.js
+++ frontend/src/components/ConnectWallet.js
@@ -3,6 +3,55 @@ import React from "react";
 import { NetworkErrorMessage } from "./NetworkErrorMessage";
 
 export function ConnectWallet({ connectWallet, networkError, dismiss }) {
+    const styles = {
+        copyrightMain: {
+            padding: "20px",
+            clear: "both",
+            fontSize: "28px"
+        },
+        copyrightLeft: {
+            float: "left"
+        },
+        copyrightRight: {
+            float: "left",
+            marginLeft: "350px"
+        },
+        buttonMain: {
+            paddingLeft: "20px",
+            paddingTop: "50px",
+            clear: "both",
+            paddingBottom: "50px"
+        },
+        buttonLeft: {
+            float: "left",
+            width: 120,
+            height: 30,
+            backgroundColor: "white",
+            borderColor: "#cecfce"
+        },
+        buttonRight: {
+            float: "left",
+            marginLeft: "100px",
+            width: 120,
+            height: 30,
+            backgroundColor: "white",
+            borderColor: "#cecfce"
+        },
+        buttonStyle: {
+            width: 190,
+            height: 50,
+            backgroundColor: "white",
+            fontSize: "20px",
+            backgroundColor: "#a3d9f5",
+            color: "black",
+            marginLeft:"600px"
+        },
+        words: {
+            fontSize: "50px",
+            marginLeft: "300px"
+        }
+      };
+
     return (
         <div className="container">
             <div className="row justify-content-md-center">
@@ -16,10 +65,11 @@ export function ConnectWallet({ connectWallet, networkError, dismiss }) {
                     )}
                 </div>
                 <div className="col-6 p-4 text-center">
-                    <p>Please connect to your wallet.</p>
+                    <p style={styles.words}>Please connect to your wallet.</p>
                     <button
                         className="btn btn-warning"
                         type="button"
+                        style={styles.buttonStyle}
                         onClick={connectWallet}
                     >
                         Connect Wallet
