c6e9ad4136790bd39bd3c75a0b77c25fb8eac9a8
Yufei Yuan <yuanyufei1999@gmail.com>
2022-08-19 11:28:13 +0200

    Add _NFTs and related functions; Add _refund() and _transferNFT()

--- contracts/a-coin.sol
+++ contracts/a-coin.sol
@@ -21,6 +21,8 @@ contract ACoin is ERC721URIStorage, Ownable {
     // Mapping from token ID to prices
     mapping(uint256 => uint256) private _prices;
 
+    mapping(address => uint256[]) private _NFTs;
+
     // The max uint256 value of solidity
     uint256 private constant MAX_INT = 2**256 - 1;
     uint256 private constant NUM_CCOIN_TO_WAIVE_COMMISSION = 100;
@@ -34,19 +36,84 @@ contract ACoin is ERC721URIStorage, Ownable {
         cCoin = new CCoin();
     }
 
-    // Anyone can mint a new token.
+    // Only owner can mint a new token.
     function mintNFT(address recipient, string memory tokenURI)
         public
+        onlyOwner
         returns (uint256)
     {
         _tokenIds.increment();
         uint256 newItemId = _tokenIds.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
+
+        _NFTs[recipient].push(newItemId);
         _forSale[newItemId] = false;
         _prices[newItemId] = MAX_INT;
 
         return newItemId;
+    }
+
+    // Anyone can mint an NFT. Consider which one to use.
+    function mintNFTAnyone(string memory tokenURI) public returns (uint256) {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _mint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
+        _NFTs[msg.sender].push(newItemId);
+        _forSale[newItemId] = false;
+        _prices[newItemId] = MAX_INT;
+
+        return newItemId;
+    }
+
+    function _addToNFTs(address recipient, uint256 tokenId) private {
+        _requireMinted(tokenId);
+        _NFTs[recipient].push(tokenId);
+    }
+
+    function _removeFromNFTs(address sender, uint256 tokenId) private {
+        _requireMinted(tokenId);
+        for (uint i = 0; i < _NFTs[sender].length; i++) {
+            if (_NFTs[sender][i] == tokenId) {
+                _NFTs[sender][i] = _NFTs[sender][_NFTs[sender].length - 1];
+                _NFTs[sender].pop();
+            }
+        }
+    }
+
+    function _transferNFT(
+        address sender,
+        address recipient,
+        uint256 tokenId
+    ) private returns (bool) {
+        _forSale[tokenId] = false;
+        _transfer(sender, recipient, tokenId);
+        _addToNFTs(recipient, tokenId);
+        _removeFromNFTs(sender, tokenId);
+        return true;
+    }
+
+    // Refund the extra ETH to the buyer
+    function _refund(
+        address recipient,
+        uint256 price,
+        uint256 commission,
+        uint256 paid
+    ) private returns (bool) {
+        uint extraFund = paid - price;
+        bool waiveCommission = cCoin.totalBalance(recipient) >=
+            NUM_CCOIN_TO_WAIVE_COMMISSION;
+        if (waiveCommission) {
+            cCoin.reduceBalance(recipient, NUM_CCOIN_TO_WAIVE_COMMISSION);
+        } else {
+            extraFund -= commission;
+        }
+        if (extraFund > 0) {
+            payable(recipient).transfer(extraFund);
+        }
+        return waiveCommission;
     }
 
     // To transfer the token ownership to another address, the owner need to pay for the commission.
@@ -62,28 +129,13 @@ contract ACoin is ERC721URIStorage, Ownable {
         );
 
         uint commission = _prices[tokenId] / COMMISSION_PERCENTAGE;
-
         require(
             msg.value >= _prices[tokenId] + commission,
             "You do not have enough ether to pay"
         );
 
-        _forSale[tokenId] = false;
-        safeTransferFrom(_msgSender(), recipient, tokenId);
-
-        // Refund the extra ETH to the buyer
-        uint extraFund = msg.value - _prices[tokenId];
-        bool waiveCommission = cCoin.totalBalance(_msgSender()) >=
-            NUM_CCOIN_TO_WAIVE_COMMISSION;
-        if (waiveCommission) {
-            cCoin.reduceBalance(_msgSender(), NUM_CCOIN_TO_WAIVE_COMMISSION);
-        } else {
-            extraFund -= commission;
-        }
-        if (extraFund > 0) {
-            payable(_msgSender()).transfer(extraFund);
-        }
-        return waiveCommission;
+        _transferNFT(_msgSender(), recipient, tokenId);
+        return _refund(recipient, _prices[tokenId], commission, msg.value);
     }
 
     function purchase(uint256 tokenId) public payable returns (bool) {
@@ -98,30 +150,14 @@ contract ACoin is ERC721URIStorage, Ownable {
             "You do not have enough ether to pay"
         );
 
-        // Get ready to transfer the token
-        _forSale[tokenId] = false;
-
         // Transfer the ETH to the original NFT owner
         payable(ownerOf(tokenId)).transfer(_prices[tokenId]);
-
         // Transfer the NFT ownership to the buyer
-        _transfer(ownerOf(tokenId), _msgSender(), tokenId);
+        _transferNFT(ownerOf(tokenId), buyer, tokenId);
         // The buyer will get a CCoin as bonus
         cCoin.mintFT(buyer);
 
-        // Refund the extra ETH to the buyer
-        uint extraFund = msg.value - _prices[tokenId];
-        bool waiveCommission = cCoin.totalBalance(buyer) >=
-            NUM_CCOIN_TO_WAIVE_COMMISSION;
-        if (waiveCommission) {
-            cCoin.reduceBalance(buyer, NUM_CCOIN_TO_WAIVE_COMMISSION);
-        } else {
-            extraFund -= commission;
-        }
-        if (extraFund > 0) {
-            payable(buyer).transfer(extraFund);
-        }
-        return waiveCommission;
+        return _refund(buyer, _prices[tokenId], commission, msg.value);
     }
 
     function getContractOwner() public view returns (address) {
@@ -157,6 +193,10 @@ contract ACoin is ERC721URIStorage, Ownable {
         );
         // The currency is in ether (rather than wei)
         _prices[tokenId] = price * 1e18;
+    }
+
+    function getNFTs() public view returns (uint256[] memory) {
+        return _NFTs[msg.sender];
     }
 
     // Interfaces for the CCoin Contract
