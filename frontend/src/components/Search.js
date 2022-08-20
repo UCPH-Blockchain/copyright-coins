cc331b87e2dffbdf305a6590de104216c70e3bc3
Yufei Yuan <yuanyufei1999@gmail.com>
2022-08-20 12:00:44 +0200

    Add ui components from semi-ui

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Toast, Card, Typography, Space} from "@douyinfe/semi-ui";
 import { Copyrights } from "./Copyrights";
 
 export class Search extends React.Component {
@@ -11,6 +12,7 @@ export class Search extends React.Component {
     }
 
     render() {
+        const { Text, Paragraph, Title } = Typography;
         const styles = {
             inputTitle: {
                 padding: "20px",
@@ -43,6 +45,27 @@ export class Search extends React.Component {
                 marginTop: "40px"
             },
         };
+        const successToast = {
+            content: 'Seach Successfully',
+            duration: 3,
+        }
+        // const onClickSearchBtn = (event) => {
+        //     event.preventDefault();
+        //     const formData = new FormData(event.target);
+        //     const publicKey = formData.get("publicKey");
+
+        //     if (publicKey) {
+        //         this.props.searchNFT(publicKey).then(
+        //             res => {
+        //                 console.log("copyrightList:", res);
+        //                 this.setState({
+        //                     copyrightList: res
+        //                 })
+        //                 Toast.success(successToast);
+        //             }
+        //         )
+        //     }
+        // }
         return (
             <div>
                 <div style={styles.mainTitle}><b>Search Copyright</b></div>
@@ -59,6 +82,7 @@ export class Search extends React.Component {
                                     this.setState({
                                         copyrightList: res
                                     })
+                                    Toast.success(successToast);
                                 }
                             )
                         }
@@ -77,27 +101,43 @@ export class Search extends React.Component {
                     <div className="form-group" style={styles.spaceButton}>
                         <input className="btn btn-primary" type="submit" value="OK" style={styles.okButton} />
                     </div>
+                    {/* <Button onClick={onClickSearchBtn}>Search</Button> */}
                 </form>
 
-                {this.state.copyrightList.map((copyright) => {
-                    return (
-                        <div className="row">
-                            <div className="col-12">
-                                {
-                                    <Copyrights
-                                        key = {copyright.tokenID}
-                                        copyRightURI={copyright.tokenURI}
-                                        tokenID={copyright.tokenId}
-                                    />
-                                }
-                            </div>
-                        </div>
-                    )
-                })}
-
-            </div>
-
-
+                {
+                    this.state.copyrightList.map((copyright) => {
+                        // return (
+                        //     <div className="row">
+                        //         <div className="col-12">
+                        //             {
+                        //                 <Copyrights
+                        //                     key={copyright.tokenID}
+                        //                     copyRightURI={copyright.tokenURI}
+                        //                     tokenID={copyright.tokenId}
+                        //                 />
+                        //             }
+                        //         </div>
+                        //     </div>
+                        // )
+                        return (
+                            <Card
+                                key={copyright.tokenID}
+                                title={copyright.tokenId}
+                                style={{ maxWidth: 360 }}
+                                shadows='always'
+                                headerExtraContent={
+                                    <Text link={{ href: 'https://www.google.com' }}>
+                                        More
+                                    </Text>
+                                }>
+                                <Space wrap>
+                                    {copyright.tokenURI}
+                                </Space>
+                            </Card>
+                        )
+                    })
+                }
+            </div >
         );
     }
 }
